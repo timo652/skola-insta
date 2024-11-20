22e199b11aa6a5e598050b4719ea545798071740
timo652 <spaghettitoucher@gmail.com>
2024-11-20 13:42:55 +0100

    gdpr_pridat

---
+++ src/app/gdpr/page.tsx
@@ -0,0 +1,13 @@
+//src/app/gdpr/page.tsx
+
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/material";
+
+
+export default function Home() {
+  return (
+        <Box>
+         <Typography>Toto je GDPR</Typography>
+        </Box>
+  );
+}
